89f17bf25ff180348b6b9692770663fc6e66e1c9
Chinmay <Chinmay8116@gmail.com>
2021-06-14 09:19:28 +0530

    added tree traversal

---
+++ Ttree Traversals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Ttree_Traversals
+{
+
+    public class BinaryTree
+    {
+        BinaryTree left;
+        BinaryTree right;
+        Int32 key;
+
+        public BinaryTree(Int32 value)
+        {
+            key = value;
+        }
+
+        public void InOrder(BinaryTree root)
+        {
+            if(root!=null)
+            {
+                InOrder(root.left);
+                Console.Write(root.key + " ");
+                InOrder(root.right);
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
