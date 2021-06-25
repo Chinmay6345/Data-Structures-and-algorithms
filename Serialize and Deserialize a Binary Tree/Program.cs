70d52c81ddb5360262aee9f2e070b4c57bc978ab
Chinmay <Chinmay8116@gmail.com>
2021-06-25 09:55:25 +0530

    commit-code

---
+++ Serialize and Deserialize a Binary Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Ttree_Traversals;
+using System.Collections.Generic;
+
+namespace Serialize_and_Deserialize_a_Binary_Tree
+{
+    public static class AppHelper
+    {
+        public static void Serialize(BinaryTree root,List<Int32>lst)
+        {
+            if(root.Equals(null))
+            {
+                lst.Add(-1);
+                return;
+            }
+            lst.Add(root.key);
+            Serialize(root.left, lst);
+            Serialize(root.right, lst);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
