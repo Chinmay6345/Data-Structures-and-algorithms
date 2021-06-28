7699f481b08fabfb878580eded3d65658b81e3d2
Chinmay <Chinmay8116@gmail.com>
2021-06-28 09:10:04 +0530

    commit-iterative in order traversal

---
+++ Iterative In-order traversal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Count_nodes_in_a_complete_Binary_Tree;
+namespace Iterative_In_order_traversal
+{
+    public static class AppHelper
+    {
+        public static void IterativeInorder(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode curr = root;
+            while(curr!=null || stack.Any())
+            {
+                while(curr!=null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.Pop();
+                Console.Write(curr.val + " ");
+                curr = curr.right;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
